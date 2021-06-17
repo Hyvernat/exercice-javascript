06cca91ead92b9824f7034b24d1f5f97cee4e141
Hyvernat <hafsa-hyvernat@hotmail.fr>
2021-06-17 10:32:36 +0200

    init

---
+++ boucle.js
@@ -0,0 +1,21 @@
+//les boucles permettend de répéter une certaine logique suivant une condition précise
+
+//La boucle While:
+//Elle permet d'exécuter un cod tant que la condition passée en paramètre n'est pas satisfaite 
+// var i = 0
+//while( i < 11){
+//console.log(i)    
+// i= i + 1
+//}
+
+//Il est possible de fircer la de la bouclee a l'aide du mot clé (break).
+//if ( i ==3){
+//break
+//}
+
+
+//La boucle For:
+//Ellle permet d'éxécuter un certzian nombre de fois en précisant manuellement l'intervale pour lequel on souhaite faire la boucle.
+//var eleves = ['Jean', 'Marc', 'Marion', 'Antoine']
+//for(var i 0; i < 3; i++){
+//console.log('Bonjour ' + eleves [i])    
