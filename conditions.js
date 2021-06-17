21d63bb6bae426bd8640a14c2a78938777b5b491
Hyvernat <hafsa-hyvernat@hotmail.fr>
2021-06-17 10:40:03 +0200

    init

---
+++ conditions.js
@@ -0,0 +1,23 @@
+//2-Les conditions:
+//comment faire pour voir le resultat de mon script?
+//a l'interirur du "script" il faut mettre 'var nom = "nom" ' puis mettre console.log(nom) , a l'interrieur du console il faut mettre le nom de la console .
+
+
+//Que sert l'utilisation des conditions?
+//Les conditions nous permet de faire des tests et de vérifier que quelque chose est juste pour pouvoir exécuté des codes particuliers.
+
+
+//Opérateur de comparaison:
+ 
+a== b //a égale à b
+a === b //a est de même "type" que b
+a >= b //a supérieur ou égal à b
+a > b //strictement supérieur 
+a <= b //inférieur ou égal à b
+a < b // a strictement inférieur a b 
+
+//Comment écrire des conditions:
+//IL y a plusieurs façons d'écrire des conditions:
+//1-if(sinon)
+//2-else(sinon)
+//3-else if (sinon si)
